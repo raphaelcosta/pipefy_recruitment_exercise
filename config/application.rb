3c5126c0b5f1b36ef27325ce663e8b129f606bbf
Joao Sampaio <joao.sampaio@sage.com>
2017-01-22 04:40:03 -0200

    fixes heroku

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,7 @@
 require_relative 'boot'
 require 'rails/all'
+# require './lib/api/updater'
+# require './lib/api/communicator'
 
 Bundler.require(*Rails.groups)
 
@@ -7,6 +9,7 @@ module PipefyRecruitmentExercise
   class Application < Rails::Application
 		# config.autoload_paths << "#{Rails.root}/lib"
     # config.autoload_paths += %W( #{config.root}/lib )
-    config.autoload_paths += Dir["#{config.root}/lib/**/"]
+    config.autoload_paths   += %W( #{config.root}/lib)
+    # config.autoload_paths += Dir["#{config.root}/lib/**/"]
   end
 end
