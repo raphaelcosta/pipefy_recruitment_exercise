360a928dd3e7c3151367e696a1b99f8866d01be0
Joao Sampaio <joao.sampaio@sage.com>
2017-01-24 01:04:45 -0200

    fixes heroku issuer

--- config/application.rb
+++ config/application.rb
@@ -5,6 +5,6 @@ Bundler.require(*Rails.groups)
 
 module PipefyRecruitmentExercise
   class Application < Rails::Application
-    config.autoload_paths   += %W( #{config.root}/lib)
+    config.autoload_paths   += %W( #{config.root}/lib) if  ['development', 'test'].include? Rails.env
   end
 end
